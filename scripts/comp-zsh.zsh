0c1622f33daf295769e8a77cd012d8f26001cb51
fioncat <lazycat7706@gmail.com>
2024-02-04 19:20:37 +0800

    fix: comp zsh failed

--- scripts/comp-zsh.zsh
+++ scripts/comp-zsh.zsh
@@ -1,8 +1,8 @@
 _kubeswitch() {
-	echo "xxxxxxxxxxxxxxxx" >> /tmp/comp-logs
-	local items=($(./target/debug/kubeswitch --comp -- "${words[@]}" 2>/tmp/.kubeswitch_comp_logs))
-	echo "${items[@]}" >> /tmp/comp-logs
+	local cmd=${words[1]}
+    local cmp_args=("${words[@]:1}")
+	local items=($($cmd --comp -- "${cmp_args[@]}" 2>/tmp/.kubeswitch_comp_logs))
 	_describe 'command' items
 }
 
-compdef ./target/debug/kubeswitch _kubeswitch
+compdef _kubeswitch kubeswitch
